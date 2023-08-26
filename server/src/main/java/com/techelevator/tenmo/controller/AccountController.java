c204085955b7ff3ca7cd7f4df9124b9cdb7f4252
Amanda Schaffer <amrschaffer@gmail.com>
2023-08-26 02:13:57 -0400

    only 1 more Path to fill

--- server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
+++ server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
@@ -1,12 +1,14 @@
 package com.techelevator.tenmo.controller;
 
 import com.techelevator.tenmo.dao.AccountDao;
+import com.techelevator.tenmo.exception.DaoException;
 import com.techelevator.tenmo.model.Account;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -18,45 +20,79 @@ public class AccountController
         this.accountDao = accountDao;
     }
 
+
+
     // create account
-    @PreAuthorize("isAuthenticated()")
-    @RequestMapping(path = "", method = RequestMethod.POST)
-    public Account createAccountByInputID(@RequestBody int userId)
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(path = "/accounts", method = RequestMethod.POST)
+    public Account createAccountByInputID(@Valid @RequestBody int userId)
     {
         return accountDao.createAccount(userId);
     }
 
+
+
+
     // get account by account ID
-    @PreAuthorize("isAuthenticated()")
-    @RequestMapping(path = "", method = RequestMethod.GET)
-    public Account findAccountByAccountId(@RequestBody int accountId)
+    @RequestMapping(path = "/accounts/{id}", method = RequestMethod.GET)
+    public Account getAccountByAccountId(@PathVariable int accountId)
     {
-        return accountDao.getAccountById(accountId);
+        Account account = accountDao.getAccountById(accountId);
+        if (account == null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Account not found.");
+        } else {
+            return account;
+        }
     }
 
+
+
+
     // get accounts by user ID
-    @PreAuthorize("isAuthenticated()")
-    @RequestMapping(path = "", method = RequestMethod.GET)
-    public List<Account> findAccountsByUserId(@RequestBody int userId)
+    @RequestMapping(path = "/users/{id}/accounts", method = RequestMethod.GET)
+    public List<Account> getAccountsByUserId(@PathVariable("id") int userId)
     {
-        return accountDao.getAccountsForUser(userId);
+        List<Account> accounts = accountDao.getAccountsForUser(userId);
+        if (accounts == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Account(s) not found.");
+        } else {
+            return accounts;
+        }
     }
+
+
+
 
     // get current bal
     @PreAuthorize("isAuthenticated()")
-    @RequestMapping(path = "", method = RequestMethod.GET)
-    public BigDecimal getCurrentBalanceByAccountId(int accountId)
+    @RequestMapping(path = "/accounts/{id}/balance", method = RequestMethod.GET)
+    public BigDecimal getCurrentBalanceByAccountId(@Valid @PathVariable int accountId)
     {
-        return accountDao.getCurrentBalance(accountId);
+        Account account = accountDao.getAccountById(accountId);
+        if (account == null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Account not found.");
+        } else {
+            return account.getBalance();
+        }
     }
+
+
+
 
     // update bal
-    @PreAuthorize("isAuthenticated()")
-    @RequestMapping(path = "", method = RequestMethod.PUT)
-    public BigDecimal updateAccountBalanceByAccountIdAndMoneyInput(int accountId, BigDecimal amountToChange)
+    @RequestMapping(path = "/accounts/{id}", method = RequestMethod.PUT)
+    public BigDecimal updateAccountBalanceByAccountIdAndMoneyInput
+        (@Valid @RequestBody Account account,
+        @PathVariable int accountId, BigDecimal amountToChange)
     {
-        return accountDao.updateAccountBalance(accountId, amountToChange);
+        account.setAccountId(accountId);
+        try {
+            BigDecimal updatedAccount = accountDao.updateAccountBalance(accountId, amountToChange);
+            return updatedAccount;
+        }
+        catch (DaoException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Account not found.");
+        }
     }
-
 
 }
--- server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
+++ server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
@@ -2,9 +2,14 @@ package com.techelevator.tenmo.controller;
 
 import com.techelevator.tenmo.dao.TransferDao;
 import com.techelevator.tenmo.model.Transfer;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.math.BigDecimal;
+import java.util.List;
 
 public class TransferController
 {
@@ -15,10 +20,42 @@ public class TransferController
         this.transferDao = transferDao;
     }
 
-    public Transfer createTransferBySenderRecipient(@RequestBody int senderAccountId, int recipientAccountId, BigDecimal transferAmount, String transferType)
+    // TODO Fill in PATH here
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("isAuthenticated()") // ????? maybe???
+    @RequestMapping(path = "", method = RequestMethod.POST)
+    public Transfer createTransferBySenderAndRecipient
+            (@Valid @RequestBody int senderAccountId, int recipientAccountId, BigDecimal transferAmount, String transferType)
     {
         return transferDao.createTransfer(senderAccountId, recipientAccountId, transferAmount, transferType);
     }
 
+
+
+
+    @RequestMapping(path = "/transfers/{id}", method = RequestMethod.GET)
+    public Transfer getTransferByTransferId(@PathVariable int transferId)
+    {
+        Transfer transfer = transferDao.getTransferById(transferId);
+        if (transfer == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Transfer not found.");
+        } else {
+            return transfer;
+        }
+    }
+
+
+
+    @RequestMapping(path = "/accounts/{id}/transfers", method = RequestMethod.GET)
+    public List<Transfer> getTransfersForAccountByAccountId(@PathVariable("id") int accountId)
+    {
+        List<Transfer> transfers = transferDao.getTransfersForAccount(accountId);
+        if (transfers == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Account not found.");
+        } else {
+            return transfers;
+        }
+
+    }
 
 }
